3fb801c0cf344072457820e2ef7ce3de28121685
jean0828 <50563197+jean0828@users.noreply.github.com>
2021-05-19 13:35:22 -0500

    Update and rename Get-DevicesFromGroup.ps1 to Get-UsersFromGroup.ps1

--- Get-DevicesFromGroup.ps1
+++
@@ -1,1 +0,0 @@
-﻿Get-AdGroupMember -identity "Active Directory Group where is the devices" -Recursive | select name, distinguishedName  | Export-csv -path "C:\export.csv" -NoTypeInformation -Encoding UTF8
---
+++ Get-UsersFromGroup.ps1
@@ -0,0 +1,1 @@
+﻿Get-AdGroupMember -identity "Group" -Recursive | select name, samaccountname  | Export-csv -path "C:\export.csv" -NoTypeInformation -Encoding UTF8
