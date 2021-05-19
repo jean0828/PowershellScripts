61e3cbf76427c658b3a50ea6adc64a9195e050a9
jean0828 <50563197+jean0828@users.noreply.github.com>
2021-05-19 13:36:34 -0500

    Add files via upload

---
+++ Get-AllDevices.ps1
@@ -0,0 +1,3 @@
+﻿$file = Read-Host -Prompt 'Input the file name'
+$filename= 'C:\users\public\'+$file + '.csv'
+Get-ADcomputer -filter '*' -Properties * | select Name, OperatingSystem,CanonicalName,LastLogonDate | Export-Csv $filename  -NoTypeInformation -Encoding UTF8
---
+++ Get-EquiposEstandaresAdmin.ps1
@@ -0,0 +1,11 @@
+﻿$nameofgroup= 'GS Equipos Sin Administradores Locales'
+$groupsusers=Get-ADGroup -Identity $nameofgroup | 
+  ForEach-Object{
+                $settings=@{Group=$_.DistinguishedName;Member=$null}
+        $_ | Get-ADGroupMember |
+              ForEach-Object{
+                                       $settings.Member=$_.DistinguishedName
+                    New-Object PsObject -Property $settings
+                }
+    }
+    $groupsusers | Export-Csv C:\Users\Public\Equiposestandar.csv –NoTypeInformation
