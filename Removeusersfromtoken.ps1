3835ef0bb86d1e8c526582cae6766be39f3d0874
jean0828 <50563197+jean0828@users.noreply.github.com>
2021-05-19 13:32:06 -0500

    compare groups AD

---
+++ Removeusersfromtoken.ps1
@@ -0,0 +1,13 @@
+﻿$exportlocation = "C:\Users\Public\$(Get-Date -UFormat '%Y%m%d')_removeusers.csv"
+$current = Get-Date
+$a= Get-ADGroupMember "VPN Acceso General" -Recursive | Get-ADUser -Properties "LastLogonDate" | Select Name,SamAccountName,LastLogonDate
+$a | ForEach-Object {
+    if (($current - $_.LastLogonDate).Days -gt 90) {
+    $string = [pscustomobject]@{
+    "Name" = $_.SamAccountName
+    "LastLogon" = $_.LastLogonDate.ToString("yyyy/MM/dd")
+    } 
+    $string | Export-Csv -Path $exportlocation -Append -NoTypeInformation 
+    }
+}
+
