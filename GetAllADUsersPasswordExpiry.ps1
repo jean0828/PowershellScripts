8d9169843c0eca73677c75b0b491dd772ad8cf62
jean0828 <50563197+jean0828@users.noreply.github.com>
2021-05-19 11:36:54 -0500

    Obtain all ADusers with  password Expiration

---
+++ GetAllADUsersPasswordExpiry.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -filter * –Properties "SamAccountName","DisplayName", "msDS-UserPasswordExpiryTimeComputed", "EmailAddress","Enabled","Company","Title","Department","PostalCode","LastLogonDate","LastBadPasswordAttempt","PasswordExpired","PasswordLastSet","badPwdCount", "CanonicalName" |
+Select-Object -Property "SamAccountName",  "EmailAddress","Enabled","Company","Title","Department","PostalCode","LastLogonDate","LastBadPasswordAttempt","PasswordExpired","PasswordLastSet","badPwdCount", "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}, "CanonicalName" |
+Export-CSV "C:\users\public\adusers20210507.csv" -NoTypeInformation -Encoding UTF8
